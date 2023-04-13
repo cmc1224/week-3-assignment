9f56f19e4f4713cb1aa52affd13121b45549a7cf
cmc1224 <cmc1224@nyu.edu>
2023-04-13 15:50:38 -0400

    Logo_icons

--- js/scripts.js
+++ js/scripts.js
@@ -42,7 +42,7 @@ const originalLocation = [
         "year_opened": "1948"
     },
     {
-        "restaurant_chain": "Starbucks ",
+        "restaurant_chain": "Starbucks",
         "address": "2000 Western Avenue, Seattle, Washington",
         "longitude": -122.3434901,
         "latitude": 47.6104552,
@@ -58,15 +58,15 @@ const originalLocation = [
         "year_opened": "1993"
     },
     {
-        "restaurant_chain": "Nathan's Famous ",
+        "restaurant_chain": "Nathan's Famous",
         "address": "1310 Surf Avenue, Brooklyn, New York",
-        "longitude": 73.9815042,
+        "longitude": -73.9815042,
         "latitude": 40.575379,
         "still_operational": "yes",
         "year_opened": "1916"
     },
     {
-        "restaurant_chain": "Wendy's ",
+        "restaurant_chain": "Wendy's",
         "address": "257 E. Broad Street, Columbus, Ohio",
         "longitude": -82.9936951,
         "latitude": 39.9627179,
@@ -74,7 +74,7 @@ const originalLocation = [
         "year_opened": "1969"
     },
     {
-        "restaurant_chain": "Hooters ",
+        "restaurant_chain": "Hooters",
         "address": "2800 Gulf-to-Bay Blvd, Clearwater, Florida",
         "longitude": -82.7212464,
         "latitude": 27.9609459,
@@ -130,7 +130,7 @@ const originalLocation = [
         "year_opened": "1959"
     },
     {
-        "restaurant_chain": "KFC ",
+        "restaurant_chain": "KFC",
         "address": "3890 S. State Street, Salt Lake City, Utah\n",
         "longitude": -111.8887561,
         "latitude": 40.6872824,
@@ -162,7 +162,7 @@ const originalLocation = [
         "year_opened": "1959"
     },
     {
-        "restaurant_chain": "Jamba ",
+        "restaurant_chain": "Jamba",
         "address": "17 Chorro Street, Suite C, San Luis Obispo, California",
         "longitude": -120.6708509,
         "latitude": 35.2936441,
@@ -178,7 +178,7 @@ const originalLocation = [
         "year_opened": "1948"
     },
     {
-        "restaurant_chain": "Tim Hortons ",
+        "restaurant_chain": "Tim Hortons",
         "address": "65 Ottawa Street N., Hamilton, Ontario, Canada",
         "longitude": -79.8191856,
         "latitude": 43.2446875,
@@ -234,7 +234,7 @@ const originalLocation = [
         "year_opened": "1967"
     },
     {
-        "restaurant_chain": "Fuddruckers ",
+        "restaurant_chain": "Fuddruckers",
         "address": "8602 Botts Lane, San Antonio, Texas",
         "longitude": -98.4601686,
         "latitude": 29.5182071,
@@ -252,36 +252,160 @@ const map = new mapboxgl.Map({
     container: 'map', // container ID
     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
     style: 'mapbox://styles/mapbox/light-v11', // style URL
-    center: [-101.43084079316468,39.12071916433891], // starting position [lng, lat]
+    center: [-101.43084079316468, 39.12071916433891], // starting position [lng, lat]
     zoom: 3, // starting zoom
     pitch: 0
 });
 
+// add markers to map
+
 originalLocation.forEach(function (_Restaurant) {
     // create the popup
-    const popup = new mapboxgl.Popup({ offset: 25}).setText(
+    const popup = new mapboxgl.Popup({ offset: 25 }).setText(
         `The original ${_Restaurant['restaurant_chain']} opened at ${_Restaurant['address']} in ${_Restaurant['year_opened']}.`
     )
 
-    // figure out the color of the marker based on the program variable
 
-    // by default, use gray
-    let color = '#ccc'
+    //logo markers
+    const el = document.createElement('div');
+    el.className = 'marker';
+        if (_Restaurant['restaurant_chain'] === "McDonald's") {
+            el.style.backgroundImage = "URL('https://logos-download.com/wp-content/uploads/2016/03/McDonalds_Logo_2018.svg')";
+        }
 
-    if (_Restaurant['still_operational'] === 'yes') {
+        if (_Restaurant['restaurant_chain'] === "Pizza Hut") {
+            el.style.backgroundImage = "URL('https://upload.wikimedia.org/wikipedia/sco/d/d2/Pizza_Hut_logo.svg')";
+        }
+
+        if (_Restaurant['restaurant_chain'] === "T.G.I. Friday's") {
+            el.style.backgroundImage = "URL('https://upload.wikimedia.org/wikipedia/commons/4/44/Tgi_fridays_logo13.svg')";
+        }
+
+        if (_Restaurant['restaurant_chain'] === "Waffle House") {
+            el.style.backgroundImage = "URL('https://www.logo.wine/a/logo/Waffle_House/Waffle_House-Logo.wine.svg')";
+        }
+
+        if (_Restaurant['restaurant_chain'] === "Dunkin' Donuts") {
+            el.style.backgroundImage = "URL('https://logotyp.us/files/dunkin-donuts.svg')";
+        }
+
+        if (_Restaurant['restaurant_chain'] === "Starbucks") {
+            el.style.backgroundImage = "URL('https://upload.wikimedia.org/wikipedia/sco/d/d3/Starbucks_Corporation_Logo_2011.svg')";
+        }
+
+        if (_Restaurant['restaurant_chain'] === "Chipotle Mexican Grill") {
+            el.style.backgroundImage = "URL('https://cdn.worldvectorlogo.com/logos/chipotle-mexican-grill.svg')";
+        }
+
+        if (_Restaurant['restaurant_chain'] === "Nathan's Famous ") {
+            el.style.backgroundImage = "URL('https://upload.wikimedia.org/wikipedia/commons/7/75/Nathan%27s_Famous.svg')";
+        }
+
+        if (_Restaurant['restaurant_chain'] === "Wendy's") {
+            el.style.backgroundImage = "URL('https://brandeps.com/logo-download/W/Wendys-logo-vector-01.svg')";
+        }
+
+        if (_Restaurant['restaurant_chain'] === "Hooters") {
+            el.style.backgroundImage = "URL('https://www.logo.wine/a/logo/Hooters/Hooters-Logo.wine.svg')";
+        }
+
+        if (_Restaurant['restaurant_chain'] === "Blimpie") {
+            el.style.backgroundImage = "URL('https://cdn.freebiesupply.com/logos/large/2x/blimpie-2-logo-svg-vector.svg')";
+        }
+
+        if (_Restaurant['restaurant_chain'] === "Taco Bell") {
+            el.style.backgroundImage = "URL('https://www.logo.wine/a/logo/Taco_Bell/Taco_Bell-Logo.wine.svg')";
+        }
+
+        if (_Restaurant['restaurant_chain'] === "Burger King") {
+            el.style.backgroundImage = "URL('https://www.logo.wine/a/logo/Burger_King/Burger_King-Logo.wine.svg')";
+        }
+
+        if (_Restaurant['restaurant_chain'] === "Sbarro's") {
+            el.style.backgroundImage = "URL('https://upload.wikimedia.org/wikipedia/commons/e/ef/Sbarro_logo.svg')";
+        }
+
+        if (_Restaurant['restaurant_chain'] === "White Castle") {
+            el.style.backgroundImage = "URL('https://static.wikia.nocookie.net/logopedia/images/1/13/White_Castle_1.svg/')";
+        }
+
+
+        if (_Restaurant['restaurant_chain'] === "Sonic") {
+            el.style.backgroundImage = "URL('https://upload.wikimedia.org/wikipedia/commons/f/ff/SONIC_New_Logo_2020.svg')";
+        }
+
+
+        if (_Restaurant['restaurant_chain'] === "KFC") {
+            el.style.backgroundImage = "URL('https://upload.wikimedia.org/wikipedia/sco/b/bf/KFC_logo.svg')";
+        }
+
+
+        if (_Restaurant['restaurant_chain'] === "Panda Express") {
+            el.style.backgroundImage = "URL('https://cdn.worldvectorlogo.com/logos/panda-express-logo-1.svg')";
+        }
+
+        if (_Restaurant['restaurant_chain'] === "Subway") {
+            el.style.backgroundImage = "URL('https://upload.wikimedia.org/wikipedia/commons/5/5c/Subway_2016_logo.svg')";
+        }
+
+        if (_Restaurant['restaurant_chain'] === "Little Caesars") {
+            el.style.backgroundImage = "URL('https://seeklogo.com/images/L/little-caesars-pizza-logo-95340DB81C-seeklogo.com.png')";
+        }
+
+        if (_Restaurant['restaurant_chain'] === "Jamba") {
+            el.style.backgroundImage = "URL('https://upload.wikimedia.org/wikipedia/commons/6/6f/Jamba_logo.svg')";
+        }
+
+        if (_Restaurant['restaurant_chain'] === "In-N-Out") {
+            el.style.backgroundImage = "URL('https://upload.wikimedia.org/wikipedia/commons/f/f2/InNOut.svg')";
+        }
+
+        if (_Restaurant['restaurant_chain'] === "Tim Hortons") {
+            el.style.backgroundImage = "URL('https://brandeps.com/logo-download/T/Tim-Hortons-logo-vector-01.svg')";
+        }
+
+        if (_Restaurant['restaurant_chain'] === "Five Guys") {
+            el.style.backgroundImage = "URL('https://upload.wikimedia.org/wikipedia/commons/8/80/Five_Guys_logo.svg')";
+        }
+
+        if (_Restaurant['restaurant_chain'] === "Dairy Queen") {
+            el.style.backgroundImage = "URL('https://upload.wikimedia.org/wikipedia/commons/a/ae/Dairy_Queen_logo.svg')";
+        }
+
+        if (_Restaurant['restaurant_chain'] === "Jack In The Box") {
+            el.style.backgroundImage = "URL('https://upload.wikimedia.org/wikipedia/commons/6/6e/Jack_in_the_Box_2009_logo.svg')";
+        }
+
+        if (_Restaurant['restaurant_chain'] === "Benihana") {
+            el.style.backgroundImage = "URL('https://cdn.worldvectorlogo.com/logos/benihana-grill.svg')";
+        }
+
+        if (_Restaurant['restaurant_chain'] === "Chick-fil-A") {
+            el.style.backgroundImage = "URL('https://cdn.cdnlogo.com/logos/c/10/chick-fil-a.svg')";
+        }
+
+        if (_Restaurant['restaurant_chain'] === "Carrabba's Italian Grill") {
+            el.style.backgroundImage = "URL('https://cdn.cdnlogo.com/logos/c/22/carrabbas.svg')";
+        }
+
+        if (_Restaurant['restaurant_chain'] === "Fuddruckers ") {
+            el.style.backgroundImage = "URL('https://upload.wikimedia.org/wikipedia/commons/4/45/Logo_of_Fuddruckers.svg')";
+    }
+    // color of the marker based on operational status
+
+    /*if (_Restaurant['still_operational'] === 'yes') {
         color = '#258f2e'
     }
 
     if (_Restaurant['still_operational'] === 'no') {
         color = '#a61c1c'
-    }
-
-
+    }*/
 
     // map center point
-    new mapboxgl.Marker({
-        color: color
-    })
+    new mapboxgl.Marker(el)
+        //{
+        //color: color
+        //})
         .setLngLat([_Restaurant.longitude, _Restaurant.latitude])
         .setPopup(popup)
         .addTo(map);
@@ -289,30 +413,30 @@ originalLocation.forEach(function (_Restaurant) {
 
 //event listeners
 
-$('#fly-to-west-coast').on('click', function() {
+$('#fly-to-west-coast').on('click', function () {
     map.flyTo({
-        center:[-116.65593160519431,42.26800326063527],
+        center: [-120.3908297401862, 38.95246033425006],
         zoom: 4,
     })
 })
 
-$('#fly-to-east-coast').on('click', function() {
+$('#fly-to-east-coast').on('click', function () {
     map.flyTo({
-        center: [-79.86803275316097, 37.80889269035391],
-        zoom: 4,
-    })
-})
-
-
-$('#fly-to-midwest').on('click', function() {
-    map.flyTo({
-        center: [-102.59639798451622,44.38735352147347],
+        center: [-75.16978486756413, 42.35864565224182],
         zoom: 4,
     })
 })
 
 
-$('#fly-to-the-south').on('click', function() {
+$('#fly-to-midwest').on('click', function () {
+    map.flyTo({
+        center: [-102.31999433098571, 41.70700950446328],
+        zoom: 4,
+    })
+})
+
+
+$('#fly-to-the-south').on('click', function () {
     map.flyTo({
         center: [-90.212352826406, 34.215645980651786],
         zoom: 4,
